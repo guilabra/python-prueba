2adc28cbd716823318533ef5bb3fc3d9056cbcdb
Guillermo Labrador <glabradorv@gmail.com>
2016-11-29 12:43:40 +0100

    modificado la funcion

---
+++ Bottles.py
@@ -0,0 +1,35 @@
+class Bottles:
+
+    def song(self):
+        return self.verses(99,0)
+
+    def verses(self, last, first):
+        o = self.verse(last)
+        for i in range (last-1, first-1, -1):
+            o += "\n" + self.verse (i) 
+        return o
+
+    def verse(self, number = None):
+        if number == 0:
+            return """No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+"""
+        elif number == 1:
+            return """1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.
+"""
+        else:
+           return """{0} bottles of beer on the wall, {0} bottles of beer.
+Take one down and pass it around, {1} {2} of beer on the wall.
+""".format(number, number-1, self.container(number-1))
+
+
+    def container(self, number):
+        if number==1:
+            return "bottle";
+        else:
+            return "bottles";
+
+
+
+        
