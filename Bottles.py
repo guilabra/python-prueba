f7b8fcf927d8f6b6e64f22b807a5902f62b92385
Guillermo Labrador <glabradorv@gmail.com>
2016-11-30 12:56:49 +0100

    modificado

--- Bottles.py
+++ Bottles.py
@@ -1,5 +1,3 @@
-from Container_Number import Container_Number
-
 class Bottles:
 
     def song(self):
@@ -12,19 +10,61 @@ class Bottles:
         return o
 
     def verse(self, number = None):
-        numero_actual= Container_Number(number)
-        numero_anterior= Container_Number(number-1)
+        bottle_number = self.bottleNumberFactory(number)
+        next_bottle_number = self.bottleNumberFactory(bottle_number.successor())
+        result = \
+            "{0} {1} of beer on the wall, ".format(bottle_number.quantity().capitalize(), bottle_number.container()) + \
+            "{0} {1} of beer.\n".format(bottle_number.quantity(), bottle_number.container()) + \
+            "{0}, ".format(bottle_number.action()) + \
+            "{0} {1} of beer on the wall.\n".format(next_bottle_number.quantity(), next_bottle_number.container())
+
+        return result
+
+    def bottleNumberFactory(self, number):
         if number == 0:
-            return """No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall.
-"""
+            return BottleZero(number)
+        elif number == 1:
+            return BottleOne(number)
         else:
-           return """{0} {3} of beer on the wall, {0} {3} of beer.
-Take {4} down and pass it around, {1} {2} of beer on the wall.
-""".format(number, numero_anterior.nomore(), numero_anterior.container(), numero_actual.container(), numero_actual.itone())
-
-
-            
+            return BottleNumber(number)
     
-    
+class BottleNumber:
+    def __init__(self, number):
+        self.number = number
+
+    def container(self):
+        return "bottles"
+
+    def quantity(self):
+        return str(self.number)
         
+    def action(self):
+        return "Take {0} down and pass it around".format(self.pronoun())
+
+    def pronoun(self):
+        return "one"
+
+    def successor(self):
+        return self.number - 1
+
+
+class BottleOne(BottleNumber):
+    def container(self):
+        return "bottle"
+
+    def pronoun(self):
+        return "it"
+
+
+class BottleZero(BottleNumber):
+    def quantity(self):
+        return "no more"
+
+    def action(self):
+        return "Go to the store and buy some more"
+
+    def successor(self):
+        return 99
+        
+
+
