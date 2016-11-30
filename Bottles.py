33ae277bd3e563e6ba2cdc40be7a710d6a28f35c
Guillermo Labrador <glabradorv@gmail.com>
2016-11-30 11:43:48 +0100

    modificado

--- Bottles.py
+++ Bottles.py
@@ -14,22 +14,29 @@ class Bottles:
             return """No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.
 """
-        elif number == 1:
-            return """1 bottle of beer on the wall, 1 bottle of beer.
-Take it down and pass it around, no more bottles of beer on the wall.
-"""
         else:
-           return """{0} bottles of beer on the wall, {0} bottles of beer.
-Take one down and pass it around, {1} {2} of beer on the wall.
-""".format(number, number-1, self.container(number-1))
-
+           return """{0} {3} of beer on the wall, {0} {3} of beer.
+Take {4} down and pass it around, {1} {2} of beer on the wall.
+""".format(number, self.nomore(number-1), self.container(number-1), self.container(number), self.itone(number))
 
     def container(self, number):
         if number==1:
-            return "bottle";
+            return "bottle";    
         else:
             return "bottles";
-
-
-
+    
+    def nomore(self, number):
+        if number==0:
+            return "no more";
+        else:
+            return number;
+        
+    def itone(self, number):
+        if number==1:
+            return "it";
+        else:
+            return "one";
+            
+    
+    
         
